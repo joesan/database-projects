782de249cb6ee5885fb385fe6c262f5a89c15565
Joe San <codeintheopen@gmail.com>
2017-08-11 13:05:52 +0200

    Update power_plant_sim_setup.sql

--- power-plant-simulator/mysql/power_plant_sim_setup.sql
+++ power-plant-simulator/mysql/power_plant_sim_setup.sql
@@ -27,14 +27,13 @@ DROP TABLE IF EXISTS user,
 /*!50503 select CONCAT('storage engine: ', @@default_storage_engine) as INFO */;
 
 CREATE TABLE organization (
-    orgId      INT             NOT NULL,
     orgName    VARCHAR(25)     NOT NULL,
     street     VARCHAR(20)     NOT NULL,
     city       VARCHAR(20)     NOT NULL,
     country    VARCHAR(20)     NOT NULL, 
     createdAt  TIMESTAMP       NOT NULL,
     updatedAt  TIMESTAMP       NOT NULL,   
-    PRIMARY KEY (orgId, orgName)
+    PRIMARY KEY (orgName)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE powerPlant (
@@ -54,13 +53,13 @@ CREATE TABLE powerPlant (
 
 CREATE TABLE user (
     userId       INT             NOT NULL,
-    orgId        INT             NOT NULL,
+    orgName        VARCHAR(25)             NOT NULL,
     firstName    VARCHAR(30)     NOT NULL,
     lastName     VARCHAR(30)     NOT NULL,
     createdAt    TIMESTAMP       NOT NULL,
     updatedAt    TIMESTAMP       NOT NULL, 
     PRIMARY KEY (userId),
-    FOREIGN KEY (orgId) REFERENCES organization (orgId) ON DELETE CASCADE
+    FOREIGN KEY (orgName) REFERENCES organization (orgName) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
@@ -70,19 +69,19 @@ CREATE TABLE user (
 -- Insert some data
 SELECT 'LOADING organization' as 'INFO';
 INSERT INTO `organization` VALUES 
-('001','Organization-001', 'street-001', 'city-001', 'Germany', TIMESTAMP('2017-08-10'), TIMESTAMP('2017-08-10')),
-('002','Organization-002', 'street-002', 'city-002', 'Germany', TIMESTAMP('2017-08-10'), TIMESTAMP('2017-08-10')),
-('003','Organization-003', 'street-003', 'city-003', 'Germany', TIMESTAMP('2017-08-10'), TIMESTAMP('2017-08-10')),
-('004','Organization-004', 'street-004', 'city-004', 'Germany', TIMESTAMP('2017-08-10'), TIMESTAMP('2017-08-10'));
+('Organization-001', 'street-001', 'city-001', 'Germany', TIMESTAMP('2017-08-10'), TIMESTAMP('2017-08-10')),
+('Organization-002', 'street-002', 'city-002', 'Germany', TIMESTAMP('2017-08-10'), TIMESTAMP('2017-08-10')),
+('Organization-003', 'street-003', 'city-003', 'Germany', TIMESTAMP('2017-08-10'), TIMESTAMP('2017-08-10')),
+('Organization-004', 'street-004', 'city-004', 'Germany', TIMESTAMP('2017-08-10'), TIMESTAMP('2017-08-10'));
 
 SELECT 'LOADING powerPlant' as 'INFO';
 INSERT INTO `powerPlant` VALUES 
-('1', 001, true,  20,  100, NULL, NULL, 'OnOffType',  TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
-('2', 001, false, 100, 800, 100,  2,    'RampUpType', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
-('3', 002, false, 200, 400, 50,   2,    'RampUpType', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
-('4', 002, true,  400, 800, NULL, NULL, 'OnOffType',  TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10'));
+('1', 'Organization-001', true,  20,  100, NULL, NULL, 'OnOffType',  TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
+('2', 'Organization-001', false, 100, 800, 100,  2,    'RampUpType', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
+('3', 'Organization-002', false, 200, 400, 50,   2,    'RampUpType', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
+('4', 'Organization-002', true,  400, 800, NULL, NULL, 'OnOffType',  TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10'));
 
 SELECT 'LOADING user' as 'INFO';
 INSERT INTO `user` VALUES 
-(1, 001, 'user', 'joe', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
-(2, 002, 'user', 'san', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10'));
+(1, 'Organization-001', 'user', 'joe', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
+(2, 'Organization-002', 'user', 'san', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10'));
