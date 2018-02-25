48fc22f97adc7d1d3e581cf08ea4cbe804921752
Joe San <codeintheopen@gmail.com>
2018-02-25 13:35:00 +0100

    Update power_plant_sim_setup.sql

--- power-plant-simulator/mysql/power_plant_sim_setup.sql
+++ power-plant-simulator/mysql/power_plant_sim_setup.sql
@@ -81,7 +81,7 @@ INSERT INTO `powerPlant` VALUES
 ('3', 'Organization-002', true,  200, 400, 50,   2,    'RampUpType', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
 ('4', 'Organization-002', true,  200, 400, 50,   2,    'RampUpType', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
 ('5', 'Organization-003', false, 200, 600, NULL, NULL, 'OnOffType',  TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
-('6', 'Organization-003', true,  400, 800, NULL, NULL, 'OnOffType',  TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10'));
+('6', 'Organization-003', true,  400, 800, NULL, NULL, 'OnOffType',  TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
 ('7', 'Organization-004', true,  100, 800, 100,  4,    'RampUpType', TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10')),
 ('8', 'Organization-004', false, 100, 900, NULL, NULL, 'OnOffType',  TIMESTAMP('2017-08-10'),  TIMESTAMP('2017-08-10'));
 
